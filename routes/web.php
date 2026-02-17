a048ae9a53984f4799b7a83285ce7a67b7ae7fff
miki-babi <mikiyasshiferaw@gmail.com>
2026-02-17 04:48:21 +0300

    storage link

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\PublicMenuController;
 use App\Http\Controllers\TelegramWebhookController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/create-storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created successfully!';
+});
 
 Route::get('/', [PublicMenuController::class, 'index'])->name('menu.index');
 Route::post('/orders', [PublicMenuController::class, 'store'])->name('menu.order.store');
